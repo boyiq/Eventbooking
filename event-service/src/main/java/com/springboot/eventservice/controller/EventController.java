dcda8d59c204eea6797ccac51792f9d614c5b314
Boyi Qu <boyiqu9596@gmail.com>
2023-03-15 17:39:26 -0700

    added get all events api endpoint and add new event api endpoint to controller

--- event-service/src/main/java/com/springboot/eventservice/controller/EventController.java
+++ event-service/src/main/java/com/springboot/eventservice/controller/EventController.java
@@ -1,13 +1,39 @@
 package com.springboot.eventservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.springboot.eventservice.model.Event;
+import com.springboot.eventservice.service.EventService;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/event")
+@RequiredArgsConstructor
 public class EventController {
 
-    @GetMapping("")
+    private final EventService eventService;
+    @GetMapping("/all")
+    public ResponseEntity<Iterable<Event>> getAllEvents() {
+        try {
+            Iterable<Event> result = eventService.getEvents();
+            return new ResponseEntity(result, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addEvent(@RequestBody Event event) {
+        try {
+            eventService.createEvent(event);
+            return new ResponseEntity<>("New event added", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Failed to add new event" + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
