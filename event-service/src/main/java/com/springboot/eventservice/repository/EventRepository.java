585ccab4aa26bb3bb94628864ac1becb964c27f9
Boyi Qu <boyiqu9596@gmail.com>
2023-03-19 23:13:09 -0700

    added findOneByName method in eventRepository

--- event-service/src/main/java/com/springboot/eventservice/repository/EventRepository.java
+++ event-service/src/main/java/com/springboot/eventservice/repository/EventRepository.java
@@ -3,5 +3,5 @@ package com.springboot.eventservice.repository;
 import com.springboot.eventservice.model.Event;
 import org.springframework.data.mongodb.repository.MongoRepository;
 public interface EventRepository extends MongoRepository<Event, String>{
-
+    Event findOneByName(String name);
 }
