e3af94fde267acc20901f8b8dfa203ce534d5f52
Boyi Qu <boyiqu9596@gmail.com>
2023-03-19 23:13:41 -0700

    implemented more tests

--- event-service/src/test/java/com/springboot/eventservice/EventServiceApplicationTests.java
+++ event-service/src/test/java/com/springboot/eventservice/EventServiceApplicationTests.java
@@ -1,6 +1,9 @@
 package com.springboot.eventservice;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.assertions.Assertions;
+import com.springboot.eventservice.repository.EventRepository;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -24,30 +27,46 @@ class EventServiceApplicationTests {
 
 	@Container
 	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
-
 	@Autowired
 	private MockMvc mockMvc;
 	@Autowired
 	private ObjectMapper objectMapper;
-
+	@Autowired
+	private EventRepository eventRepository;
 
 	@DynamicPropertySource
 	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
 		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
 	}
  	@Test
-	void shouldAddEvent() throws Exception{
-		String mappedNewEvent = objectMapper.writeValueAsString(newEvent);
+	void shouldAddEvent() throws Exception {
+		String mappedNewEvent = objectMapper.writeValueAsString(newEvent1);
 		mockMvc.perform(MockMvcRequestBuilders.post("/event")
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(mappedNewEvent)).andExpect(status().isCreated());
+		Assertions.assertTrue(eventRepository.findAll().size() ==1 );
+		Assertions.assertNotNull(eventRepository.findOneByName("World Cup final"));
 	}
 
-	private Event newEvent = Event.builder()
+	@Before
+	void setUp() {
+
+	}
+	@Test
+	void shouldGetAllEvents() throws Exception {
+
+	}
+
+	private Event newEvent1 = Event.builder()
 				.name("World Cup final")
 				.address("Sports stadium")
 				.city("London")
 				.price(200)
 				.build();
-
+	private Event newEvent2 = Event.builder()
+			.name("Art show")
+			.address("City gallery")
+			.city("New York City")
+			.price(50)
+			.build();
 }
