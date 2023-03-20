00674512ee731b3c7ffb990aa9c4af0ef6bbb6d7
Boyi Qu <boyiqu9596@gmail.com>
2023-03-19 17:10:00 -0700

    created integration test for post api

--- event-service/src/test/java/com/springboot/eventservice/EventServiceApplicationTests.java
+++ event-service/src/test/java/com/springboot/eventservice/EventServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.springboot.eventservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,6 +13,8 @@ import org.springframework.http.MediaType;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import com.springboot.eventservice.model.Event;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @SpringBootTest
@@ -24,16 +27,27 @@ class EventServiceApplicationTests {
 
 	@Autowired
 	private MockMvc mockMvc;
+	@Autowired
+	private ObjectMapper objectMapper;
+
 
 	@DynamicPropertySource
 	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
 		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
 	}
  	@Test
-	void shouldAddEvent() {
-		mockMvc.perform(MockMvcRequestBuilders.post("/event"))
+	void shouldAddEvent() throws Exception{
+		String mappedNewEvent = objectMapper.writeValueAsString(newEvent);
+		mockMvc.perform(MockMvcRequestBuilders.post("/event")
 				.contentType(MediaType.APPLICATION_JSON)
-				.content();
+				.content(mappedNewEvent)).andExpect(status().isCreated());
 	}
+
+	private Event newEvent = Event.builder()
+				.name("World Cup final")
+				.address("Sports stadium")
+				.city("London")
+				.price(200)
+				.build();
 
 }
