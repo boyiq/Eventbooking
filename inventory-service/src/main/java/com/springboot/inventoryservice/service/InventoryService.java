19e1bb41a413ae5d2dd36c8723322bfb1232cc8c
Boyi Qu <boyiqu9596@gmail.com>
2023-03-22 23:37:22 -0700

    created inventoryService

---
+++ inventory-service/src/main/java/com/springboot/inventoryservice/service/InventoryService.java
@@ -0,0 +1,37 @@
+package com.springboot.inventoryservice.service;
+
+import com.springboot.inventoryservice.model.Inventory;
+import com.springboot.inventoryservice.model.InventoryRes;
+import com.springboot.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+    private final InventoryRepository inventoryRepository;
+
+    public List<InventoryRes> checkStock (List<String> eventCodes) {
+        List<InventoryRes> resultList = new ArrayList<>();
+        for (String eachEventCode:eventCodes) {
+            Inventory inventory = inventoryRepository.findByEventCode(eachEventCode);
+            boolean isInStock;
+            if (inventory != null) {
+                isInStock = inventory.getStock_quantity() > 0;
+            } else {
+                isInStock = false;
+            }
+
+            InventoryRes result = InventoryRes.builder()
+                    .event_code(inventory.getEvent_code())
+                    .inStock(isInStock)
+                    .build();
+            resultList.add(result);
+        }
+        return resultList;
+    }
+
+}
